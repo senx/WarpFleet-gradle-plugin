535623a4ebb7c15392c1ccade6c29cd95eec45e2
Xavier Marin <xavier.marin@senx.io>
2023-02-14 15:12:12 +0100

    Tests and fixes

--- warpfleet-gradle-plugin/src/test/java/io/warp10/warpfleet/tests/TestDoc.java
+++ warpfleet-gradle-plugin/src/test/java/io/warp10/warpfleet/tests/TestDoc.java
@@ -27,7 +27,6 @@ import java.util.Objects;
 
 import static org.gradle.testkit.runner.TaskOutcome.SUCCESS;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TestDoc extends AbstractTests {
